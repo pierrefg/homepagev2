1f800c00a2d9e0140495a5836beb3ac0e54232b3
Pierre FG <pierrefg@proton.me>
2024-08-31 15:27:37 +0200

    Updates sitemap

--- webapp/next-sitemap.config.js
+++ webapp/next-sitemap.config.js
@@ -4,15 +4,15 @@
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://www.pierrefg.com',
     generateRobotsTxt: true,
-    // changefreq: 'monthly',
-    // priority: 0.7,
-    // sitemapSize: 5000,
     transform: async (config, path) => {
         const priorityMap = {
             '/': 1.0,
             '/art': 0.8,
             '/science': 0.8,
-            '/background': 0.8
+            '/background': 0.8,
+            '/about': 0.8,
+            '/legals': 0.1,
+            '/privacy': 0.1
         };
         
         const defaultPriority = 0.5;
